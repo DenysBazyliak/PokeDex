77b72bad123f6e73734f3f6d12e2c0dde8d42092
Denys Bazyliak <franciskdelyamon@gmail.com>
2025-06-02 20:44:39 +0200

    Add PokemonType function onMouse functionality and types separator

--- src/components/PokemonList/PokemonItem/PokemonItem.jsx
+++ src/components/PokemonList/PokemonItem/PokemonItem.jsx
@@ -1,13 +1,37 @@
-import React from "react";
-import style from "./PokemonItem.module.css";
-import { getNewPokemon } from "../../../store/pokeListReducer";
-import { useDispatch } from "react-redux";
+import React, { useState } from 'react';
+import style from './PokemonItem.module.css';
+import { getNewPokemon } from '../../../store/pokeListReducer';
+import { useDispatch } from 'react-redux';
+
+const PokemonType = ({ key, name }) => {
+  return (
+    <>
+      <div key={key} className={style.pokeType}>
+        {name}
+      </div>
+    </>
+  );
+};
+
 const PokemonItem = (pokemon) => {
+
+  let i = 1;
+
+  const typesLength = pokemon.types.length;
+
+  const [showType, setShowType] = useState(false);
+
   let dispatch = useDispatch();
   return (
-    <div className={style.itemWrapper}>
+    <div className={style.pokeItemWrapper}>
       <div
         className={style.pokeItem}
+        onMouseEnter={() => {
+          setShowType(true);
+        }}
+        onMouseLeave={() => {
+          setShowType(false);
+        }}
         onClick={() => {
           dispatch(getNewPokemon(pokemon));
         }}
@@ -23,18 +47,24 @@ const PokemonItem = (pokemon) => {
         <div className={style.pokeName}>
           {pokemon.name.charAt(0).toUpperCase() + pokemon.name.slice(1)}
         </div>
-        {/*<div className={style.pokeWrapper}>*/}
-        {/*  /!*${style[el.type.name]}*!/*/}
-        {/*  {pokemon.types.map((el) => {*/}
-        {/*    return (*/}
-        {/*      <div key={el.slot} className={style.pokeType}>*/}
-        {/*        {el.type.name}*/}
-        {/*      </div>*/}
-        {/*    );*/}
-        {/*  })}*/}
-        {/*</div>*/}
+      </div>
+      <div className={style.pokeTypeWrapper}>
+        {pokemon.types.map((el) => {
+          if (i < typesLength) {
+            i++;
+            return (
+              <>
+                <PokemonType key={el.slot} name={el.type.name} />
+                <div className={style.separator} />
+              </>
+            );
+          } else {
+            return <PokemonType key={el.slot} name={el.type.name} />;
+          }
+        })}
       </div>
     </div>
   );
 };
+
 export default PokemonItem;
