50f2c7b90120d5bc1ec6dec0fc1e69f532d49bbc
Denys Bazyliak <franciskdelyamon@gmail.com>
2025-06-04 18:20:20 +0200

    Fix onClick dimmingDiv issue

--- src/components/PokemonInfo/PokemonInfo.jsx
+++ src/components/PokemonInfo/PokemonInfo.jsx
@@ -6,6 +6,7 @@ import { getNewPokemon } from '../../store/pokeListReducer';
 
 const PokemonInfo = () => {
     const [mouseEnter, setMouseEnter] = useState(false);
+    const [copied, setCopied] = useState(false);
     const [showInfo, setShowInfo] = useState(false);
     const pokemon = useSelector((state) => state.pokeListReducer.pokemon);
 
@@ -15,32 +16,39 @@ const PokemonInfo = () => {
         if (pokemon)setShowInfo(true);
     }, [pokemon]);
 
-    const handleClick = () => {
-        setShowInfo(false); // triggers CSS transition
+    const handleSetNull = () => {
+        setShowInfo(false);
         setTimeout(() => {
             dispatch(getNewPokemon(null));
-        }, 300); // match your CSS transition duration
+        }, 300);
+    };
+
+    const handleCopy = () => {
+        setCopied(true);
+        navigator.clipboard.writeText(pokemon.name).then(() => setTimeout(() => setCopied(false), 2000));
+
     };
 
     if (!pokemon) {
         return null;
     } else {
         return (
-           <div onClick={() => handleClick()}
-                className={style.dimmingDiv}>
-               <div  className={style.pokeInfoItemsWrapper}>
-                   <div  className={`${style.pokeInfoItem} ${style.pokeItem} ${showInfo && style.pokeItemActive}`}>
-                       <div className={style.pokeImg} onMouseEnter={() => setMouseEnter(true)}
-                            onMouseLeave={() => setMouseEnter(false)}>
-                           <img alt={pokemon.name} width={220} height={220}
-                                src={`${mouseEnter ? pokemon.sprites.versions['generation-v']['black-white'].animated.front_shiny : pokemon.sprites.other.showdown.front_shiny}`} />
+           <>
+               <div onClick={() => handleSetNull()}
+                    className={style.dimmingDiv}> </div>
+                   <div className={style.pokeInfoItemsWrapper}>
+                       <div className={`${style.pokeInfoItem} ${style.pokeItem} ${showInfo && style.pokeItemActive}`}>
+                           <div className={style.pokeImg} onMouseEnter={() => setMouseEnter(true)}
+                                onMouseLeave={() => setMouseEnter(false)}>
+                               <img alt={pokemon.name} width={220} height={220}
+                                    src={`${mouseEnter ? pokemon.sprites.versions['generation-v']['black-white'].animated.front_shiny : pokemon.sprites.other.showdown.front_shiny}`} />
+                           </div>
+                           <div className={style.pokeName}>
+                               {pokemon.name.charAt(0).toUpperCase() + pokemon.name.slice(1) + " " + hasher(pokemon.id)}
+                           </div>
                        </div>
-                       <div className={style.pokeName}>
-                           {pokemon.name.charAt(0).toUpperCase() + pokemon.name.slice(1) + " " + hasher(pokemon.id)}
-                       </div>
-                   </div>
-                   <div
-                      className={`${style.pokeInfoItem} ${style.pokeTable} ${showInfo && style.pokeTableActive}`}>
+                       <div
+                          className={`${style.pokeInfoItem} ${style.pokeTable} ${showInfo && style.pokeTableActive}`}>
                            <table className={style.table}>
                                <tbody className={style.tableBody}>
                                <tr>
@@ -49,22 +57,22 @@ const PokemonInfo = () => {
                                </tr>
                                <tr>
                                    <th>Weight</th>
-                                   <td>{pokemon.weight /10} kg</td>
+                                   <td>{pokemon.weight / 10} kg</td>
                                </tr>
                                {pokemon.stats.map((el) => {
                                    return (
-                                       <tr key={ el.stat.name}>
-                                           <th>{el.stat.name.charAt(0).toUpperCase() + el.stat.name.slice(1)}</th>
-                                           <td>{el.base_stat}</td>
-                                       </tr>
+                                      <tr key={el.stat.name}>
+                                          <th>{el.stat.name.charAt(0).toUpperCase() + el.stat.name.slice(1)}</th>
+                                          <td>{el.base_stat}</td>
+                                      </tr>
                                    );
                                })}
                                </tbody>
                            </table>
+                       </div>
                    </div>
+           </>
 
-               </div>
-           </div>
         );
     }
 };
