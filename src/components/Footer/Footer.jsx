907137ed5672a66a618bb28e38b7ba8e541fc33d
Denys Bazyliak <franciskdelyamon@gmail.com>
2025-06-07 12:03:14 +0200

    Adjust back to top functionality

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,13 +1,34 @@
-import React from "react";
+import React, { useEffect, useState } from 'react';
 import style from "./Footer.module.css";
 import { useDispatch, useSelector } from "react-redux";
 import { loadNewPokemons } from "../../store/pokeListReducer";
 import { Preloader } from "../../utilities/Preloader/Preloader";
+
 export const Footer = () => {
   const next = useSelector((state) => state.pokeListReducer.next);
   const isLoading = useSelector((state) => state.pokeListReducer.isLoading);
-  console.log('isLoading', isLoading);
   const dispatch = useDispatch();
+
+   const [showButton, setShowButton] = useState(false);
+   useEffect(()=>{
+      const handleScroll = () => {
+         setShowButton(window.scrollY > 500);
+      };
+      window.addEventListener('scroll', handleScroll);
+      return () => {
+         window.removeEventListener('scroll', handleScroll);
+      };
+   })
+
+   const scrollToTop = () => {
+      const scrollToTop = () => {
+         if (window.scrollY !== 0) {
+            window.scrollBy(0, -75);
+            requestAnimationFrame(scrollToTop);
+         }
+      };
+      scrollToTop();
+   };
 
   if (isLoading) {
     return <Preloader />;
@@ -25,6 +46,7 @@ export const Footer = () => {
           Load Pokemons
         </button>
       </div>
+      <button className={`${style.scrollButton} ${showButton && style.showScrollButton}`} onClick={()=>scrollToTop()} title={`Scroll to top`}> ↑ </button>
     </div>
   );
 };
